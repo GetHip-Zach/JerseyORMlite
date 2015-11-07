de18680cfebb23c8724549161b9daa2693c3192e
zach-merrill <zach_merrill@orfmerrzmacx10.gallup.com>
2015-11-07 11:46:19 -0600

    Added employee resource

--- src/main/java/com/gallup/gethip/resources/EmployeeResource.java
+++ src/main/java/com/gallup/gethip/resources/EmployeeResource.java
@@ -5,7 +5,11 @@ import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -23,7 +27,7 @@ public class EmployeeResource {
     // The Java method will process HTTP GET requests
     @GET 
     // The Java method will produce content identified by the MIME Media
-    // type "text/plain"
+    // type "application/json"
     @Produces("application/json")
     public Employee getIt(@QueryParam("empno") String empno) {
     	Employee emp = null;
@@ -40,6 +44,58 @@ public class EmployeeResource {
 		}
     	return emp;
         
+    }
+    
+    @DELETE
+    @Produces("text/plain")
+    public String deleteEmployee(@QueryParam("empno") String empno){
+    	try {
+			int num = getDao().deleteById(empno);
+			if(num == 1){
+				return "Delete successful";
+			}else{
+				return "Unable to delete";
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return "Error in sql statement";
+		}
+    }
+    
+    @POST
+    @Produces("application/json")
+    @Consumes("application/json")
+    public Employee createEmployee(Employee emp){
+    	try {
+			Employee empPrime = getDao().createIfNotExists(emp);
+			if(empPrime == null){
+				// handle error
+			}else{
+				return empPrime;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	return  null;
+    }
+    
+    @PUT
+    @Produces("text/plain")
+    @Consumes("application/json")
+    public String updateEmployee(Employee emp){
+    	try {
+			int num = getDao().update(emp);
+			if(num == 1){
+				return "Update successful for employee " + emp.getEmpNo();
+			}else{
+				return "Could not update " + emp.getEmpNo();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "Error updating " + emp.getEmpNo();
+		}
     }
     
     private Dao<Employee, String> getDao(){
